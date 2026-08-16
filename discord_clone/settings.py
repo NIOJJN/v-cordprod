ca1b9a701df2a17a1b6e06ae16e6d9e3a09efe76
varaGM <151998626+NIOJJN@users.noreply.github.com>
2026-08-16 15:07:17 +0300

    Update settings.py

--- discord_clone/settings.py
+++ discord_clone/settings.py
@@ -7,8 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-your-secret-key-here'
 
 DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['v-cordprod.relaxdev.ru', '.relaxdev.ru']  # Было ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
